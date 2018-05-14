57582a39407694abedcb71bf5b29752931c1ec5e
vpoturaev <viacheslav.poturaev@lazada.com>
2018-05-14 17:43:22 +0700

    ...

---
+++ tests/src/Tmp/Swagger/MaxItems.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file ATTENTION!!! The code below was carefully crafted by a mean machine.
+ * Please consider to NOT put any emotional human-generated modifications as the splendid AI will throw them away with no mercy.
+ */
+
+namespace Swaggest\PhpCodeBuilder\Tests\Tmp\Swagger;
+
+use Swaggest\JsonSchema\Constraint\Properties;
+use Swaggest\JsonSchema\Context;
+use Swaggest\JsonSchema\Schema;
+use Swaggest\JsonSchema\Structure\ClassStructure;
+
+
+/**
+ * @method static int import($data, Context $options=null)
+ */
+class MaxItems extends ClassStructure {
+	/**
+	 * @param Properties|static $properties
+	 * @param Schema $ownerSchema
+	 */
+	public static function setUpProperties($properties, Schema $ownerSchema)
+	{
+		$ownerSchema->type = Schema::INTEGER;
+		$ownerSchema->minimum = 0;
+		$ownerSchema->setFromRef('http://json-schema.org/draft-04/schema#/definitions/positiveInteger');
+		$ownerSchema->setFromRef('#/definitions/maxItems');
+	}
+}
